260a77f2b26cb8d1f0be68708888d3db19e3e5ab
SalimTP <mss4170a@ens.fsi>
2023-04-20 16:57:12 +0200

    FizzBuzz + refactor

--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -10,7 +10,7 @@ class FizzBuzzTest {
 	void fizz_un() {
 		assertEquals(FizzBuzz.fizzbuzz(1), "1");
 	}
-	
+
 	@Test
 	void fizz_deux() {
 		assertEquals(FizzBuzz.fizzbuzz(2), "2");
@@ -20,23 +20,30 @@ class FizzBuzzTest {
 	void fizz_trois() {
 		assertEquals(FizzBuzz.fizzbuzz(3), "fizz");
 	}
-	
+
 	@Test
-	void quelques_valeurs() {
+	void fizz_quelques_valeurs() {
 		assertEquals(FizzBuzz.fizzbuzz(4), "4");
 		assertEquals(FizzBuzz.fizzbuzz(7), "7");
 	}
-	
+
 	@Test
 	void fizz_cinq() {
 		assertEquals(FizzBuzz.fizzbuzz(5), "buzz");
 	}
-	
+
 	@Test
 	void fizz_multiples() {
 		assertEquals(FizzBuzz.fizzbuzz(6), "fizz");
 		assertEquals(FizzBuzz.fizzbuzz(10), "buzz");
 		assertEquals(FizzBuzz.fizzbuzz(9), "fizz");
 		assertEquals(FizzBuzz.fizzbuzz(400), "buzz");
+	}
+
+	@Test
+	void fizz_multiples_doubles() {
+		assertEquals(FizzBuzz.fizzbuzz(15), "fizzbuzz");
+		assertEquals(FizzBuzz.fizzbuzz(2 * 3 * 5), "fizzbuzz");
+		assertEquals(FizzBuzz.fizzbuzz(3 * 3 * 5 * 5), "fizzbuzz");
 	}
 }
